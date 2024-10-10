0e084d3f98b8ac7052feddf4a1fac03e5b2fc261
Doan Tuan Khai <khai.lumberjack@gmail.com>
2024-10-10 12:30:14 +0700

    Update project

---
+++ FA24_SE1702_PRN221_G1_KDOS/KDOS.Data/UnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using KDOS.Data.Models;
+using KDOS.Data.Repository;
+
+namespace KDOS.Data
+{
+    public class UnitOfWork
+    {
+        private FA24_SE1702_211_G1_KDOSContext _unitOfWorkContext;
+        private AccountRepository _accountRepository;
+        public UnitOfWork()
+        {
+            _unitOfWorkContext = new FA24_SE1702_211_G1_KDOSContext();
+        }
+    }
+}
